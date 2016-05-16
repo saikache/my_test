3794b0bc5d97ad8c5d6d51fdc2dc020a76457d9a
saisagar <saisagar@sageabletech.com>
2016-05-16 23:43:38 +0530

    get_related_stories js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protect_from_forgery
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  protected
+
+  def json_request?
+  	# byebug
+    request.format.json?  || request.format.js?
+  end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,33 +1,24 @@
 class HomeController < ApplicationController
-    require 'open-uri'
+  require 'open-uri'
+  before_filter :get_side_bar
+
   def index
-
     @stories = JSON.load(open("http://sketches.quintype.com/api/v1/stories"))
-
-    @side_linkes_json = JSON.load(open("http://sketches.quintype.com/api/v1/config"))
-
-    @side_linkes = @side_linkes_json['layout']['menu']#.collect{|s| [s['title'],s['section-slug']]}
-
   end
-
 
   def get_related_stories
-
-   @stories = JSON.load(open("http://sketches.quintype.com/api/v1/stories?section=Pathankot Attack"))
-
-
+    @a = JSON.load(open("http://sketches.quintype.com/api/v1/stories?section=Business"))
+    respond_to do |format|
+      format.js  { render json: @a }
+      format.html
+    end
   end
 
+  private
 
-def hi
-    @a = 'sai'
-
-
-respond_to do |format|
-  format.js # actually means: if the client ask for js -> return file.js
-end
-
-    
-end
+  def get_side_bar
+    @side_linkes_json = JSON.load(open("http://sketches.quintype.com/api/v1/config"))
+    @side_linkes = @side_linkes_json['layout']['menu']#.collect{|s| [s['title'],s['section-slug']]}
+  end
 
 end
